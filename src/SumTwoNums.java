fcc14715fbc825f6cd1d592b7dce15d1de6f508c
wangwenwenying <858900953@qq.com>
2019-07-31 11:10:00 +0800

    两个数相加，简单模式

--- src/SumTwoNums.java
+++ src/SumTwoNums.java
@@ -2,6 +2,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SumTwoNums {
+    /** 
+    * @Description: 暴力解除，使用两次循环对比。 即从当前位置每次都与后面的进行对比，直到找出结果。
+    * @Param: [nums, target] 
+    * @return: int[] 
+    * @Author: www
+    * @Date: 2019/7/30 
+    */ 
     public int[] twoSum(int[] nums, int target) {
         int[] index = new int[2];
         for( int i=0; i<nums.length; i++){
@@ -16,7 +23,14 @@ public class SumTwoNums {
         return index ;
     }
 
-
+    /**
+    * @Description: 两遍哈希表，使用hashMap,其中使用两次循环，第一次将所有的值put进hashmap的key中,然后第二次循环先取出原有值与target的差值，然后看差值是否包含在map中，如果包含，而且差值所对应的value不等于i，
+     * 即：两个数不是同一个数，不在同一位置。
+    * @Param: [nums, target]
+    * @return: int[]
+    * @Author: www
+    * @Date: 2019/7/31
+    */
     public int[] twoSum2(int[] nums, int target) {
         int[] index = new int[2];
         Map<Integer,Integer> map = new HashMap<>() ;
@@ -33,10 +47,28 @@ public class SumTwoNums {
         }
         return index ;
     }
+    /**
+    * @Description: 一遍hash ，边插入，边对比。
+    * @Param: [nums, target]
+    * @return: int[]
+    * @Author: www
+    * @Date: 2019/7/31
+    */
+    public int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 
     public static void main(String[] args) {
         SumTwoNums sumTwoNums = new SumTwoNums() ;
-        int[] index = sumTwoNums.twoSum2(new int[]{2, 7, 11, 15},9) ;
+        int[] index = sumTwoNums.twoSum3(new int[]{2, 7, 11, 15},9) ;
         for(int i=0; i<index.length; i++){
             System.out.println(index[i]);
         }
